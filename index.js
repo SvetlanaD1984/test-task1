e904ee35b6d3d595cea640e3bbb6232eb5b77f39
SvetlanaD1984 <SvetlanaD1984@yandex.ru>
2024-07-12 15:27:50 +0500

    fix: changes js-script and finish

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const squares = document.querySelectorAll(".square");
 const colors = ["#ff0000", "#0500ff", "#52ff00"];
 
-let i = 0;
-setInterval(function () {
-  squares.forEach((square) => {
-    square.style.backgroundColor = colors[i];
-    if (i === colors.length) {
+squares.forEach((square) => {
+  let i = 0;
+  setInterval(function () {
+    let randomColor = colors[Math.floor(Math.random() * colors.length)];
+    square.style.backgroundColor = randomColor;
+    i++;
+    if (i == colors.length) {
       i = 0;
     }
-    i++;
-  });
-}, 1500);
-
-console.log(colors[i]);
+  }, 1500);
+});
